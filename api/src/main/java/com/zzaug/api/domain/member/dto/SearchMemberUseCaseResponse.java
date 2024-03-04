9a46ff2bed2f1147196477a2b37570220ce1642b
belljun3395 <195850@jnu.ac.kr>
2024-03-05 01:04:09 +0900

    feat: 추가 SearchMemberUseCaseResponse#notExistSearchTarget
    notExistSearchTarget: 검색 대상이 존재하지 않을 때 반환하는 응답
    
    Ref: #7

--- api/src/main/java/com/zzaug/api/domain/member/dto/SearchMemberUseCaseResponse.java
+++ api/src/main/java/com/zzaug/api/domain/member/dto/SearchMemberUseCaseResponse.java
@@ -6,6 +6,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.apache.logging.log4j.util.Strings;
 
 @Getter
 @ToString
@@ -18,4 +19,13 @@ public class SearchMemberUseCaseResponse {
 	private Long id;
 	private String email;
 	private String github;
+
+	/** 검색 대상이 존재하지 않을 때 반환하는 응답 */
+	public static SearchMemberUseCaseResponse notExistSearchTarget() {
+		return SearchMemberUseCaseResponse.builder()
+				.id(Long.MIN_VALUE)
+				.email(Strings.EMPTY)
+				.github(Strings.EMPTY)
+				.build();
+	}
 }
