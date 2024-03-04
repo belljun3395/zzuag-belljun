73a8a783eb349bde4313c9dcdbb0467d1329acde
belljun3395 <195850@jnu.ac.kr>
2024-03-04 22:48:11 +0900

    refactor: 수정 TryCountElement가 처음 생성되는 경우
    기존에 처음 생성되는 경우에 대한 코드를 작성하였지만 부족한 부분이 있어 보충하였습니다.
    
    - 처음 생성되는 경우 tryCount를 상수로 선언하여 명시하였습니다. (NEW_STATE_TRY_COUNT)
    - 처음 생성된 객체임을 확인하는 검증을 수행합니다.
        - 검증: emailAuthLogId가 null 인지 확인합니다.
    - tryCount를 plus할 때 처음 생성된 객체인지 확인하는 검증을 추가하여 잘못 사용되는 경우를 방지하려 합니다.
        - 잘못사용되는 사례: 처음 생성된 객체에서 plus를 2번 이상 하는 경우
    
    Ref: #5, #10

--- api/src/main/java/com/zzaug/api/domain/member/model/auth/TryCountElement.java
+++ api/src/main/java/com/zzaug/api/domain/member/model/auth/TryCountElement.java
@@ -16,8 +16,15 @@ import org.springframework.lang.Nullable;
 @NoArgsConstructor
 @Builder(toBuilder = true)
 public class TryCountElement {
-	private int tryCount;
+
+	private static final int NEW_STATE_TRY_COUNT = 0;
+
+	@Builder.Default private int tryCount = NEW_STATE_TRY_COUNT;
 	@Nullable private Long emailAuthLogId;
+
+	public static TryCountElement newState() {
+		return TryCountElement.builder().build();
+	}
 
 	/**
 	 * 처음 생성된 객체인지 확인합니다.
@@ -25,12 +32,29 @@ public class TryCountElement {
 	 * @return 처음 생성된 객체인지 여부
 	 */
 	public boolean isNew() {
-		return Objects.isNull(emailAuthLogId);
+		if (!Objects.isNull(emailAuthLogId)) {
+			return false;
+		}
+		return true;
 	}
 
 	/** 시도 횟수를 증가시킵니다. */
 	public void plus() {
-		assert !isNew();
+		if (isNew()) {
+			if (isTryCountNewState()) {
+				throw new IllegalStateException("tryCount must be 0 when state is new");
+			}
+			doPlus();
+			return;
+		}
+		doPlus();
+	}
+
+	private void doPlus() {
 		this.tryCount = this.tryCount + 1;
+	}
+
+	private boolean isTryCountNewState() {
+		return !Objects.equals(tryCount, NEW_STATE_TRY_COUNT);
 	}
 }
