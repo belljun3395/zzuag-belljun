f3034d6def54182c2dc31c8cc49b061145cf01c0
belljun3395 <195850@jnu.ac.kr>
2024-03-04 22:59:09 +0900

    refactor: 분리 구현 기존 SaveEmailAuthHistoryCommand 인터페이스로
    테스트 프로필에서 분리하기 위해 해당 작업이 필요하였습니다.
    
    Ref: #5, #10

---
+++ api/src/main/java/com/zzaug/api/domain/member/service/history/SaveEmailAuthHistoryCommandImpl.java
@@ -0,0 +1,60 @@
+package com.zzaug.api.domain.member.service.history;
+
+import com.zzaug.api.domain.member.dao.history.EmailAutHistoryDao;
+import com.zzaug.api.domain.member.data.entity.history.EmailAuthHistoryEntity;
+import com.zzaug.api.domain.member.model.auth.TryCountElement;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Profile("!usecase-test")
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SaveEmailAuthHistoryCommandImpl implements SaveEmailAuthHistoryCommand {
+
+	private final EmailAutHistoryDao emailAutHistoryDao;
+
+	@Override
+	@Transactional
+	public EmailAuthHistoryEntity execute(
+			TryCountElement tryCount, Long memberId, Long emailAuthId, String reason) {
+		if (tryCount.isNew()) {
+			return save(
+					tryCount.getEmailAuthLogId(),
+					memberId,
+					emailAuthId,
+					reason,
+					(long) tryCount.getTryCount());
+		} else {
+			return save(memberId, emailAuthId, reason, (long) tryCount.getTryCount());
+		}
+	}
+
+	private EmailAuthHistoryEntity save(
+			Long memberId, Long emailAuthId, String reason, Long tryCount) {
+		EmailAuthHistoryEntity emailAuthHistoryEntity =
+				EmailAuthHistoryEntity.builder()
+						.memberId(memberId)
+						.emailAuthId(emailAuthId)
+						.reason(reason)
+						.tryCount(tryCount)
+						.build();
+		return emailAutHistoryDao.saveEmailAuthLog(emailAuthHistoryEntity);
+	}
+
+	private EmailAuthHistoryEntity save(
+			Long id, Long memberId, Long emailAuthId, String reason, Long tryCount) {
+		EmailAuthHistoryEntity emailAuthHistoryEntity =
+				EmailAuthHistoryEntity.builder()
+						.id(id)
+						.memberId(memberId)
+						.emailAuthId(emailAuthId)
+						.reason(reason)
+						.tryCount(tryCount)
+						.build();
+		return emailAutHistoryDao.saveEmailAuthLog(emailAuthHistoryEntity);
+	}
+}
